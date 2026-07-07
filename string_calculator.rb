2abd214d4bb811750c963cc45b687be073c466a4
Krishna <krishna@incubyte.co>
2026-07-07 14:10:54 +0530

    Support Multi-delimeter - Green

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,17 +5,19 @@ class StringCalculator
   NEWLINE_IDENTIFIER = '/n'
   PARSER_REGEX = %r{\A//(.+?)\n(.*)}m
   MAX_PERMITTED_INTEGER = 1000
+  MULTI_DELIMITER_REGEX = /\[([^\[\]]+)\]/
 
   def initialize(string)
     @string = string
   end
 
   def execute
-    delimiter, parsed_string = parse_string
+    parsed_string = parse_string
 
     multiline_strings = parsed_string.split(NEWLINE_IDENTIFIER)
+
     multiline_strings.sum do |string|
-      integer_array = string.split(delimiter).map(&:to_i)
+      integer_array = string.split(DEFAULT_DELIMITER).map(&:to_i)
       negative_numbers = integer_array.select(&:negative?)
       raise Errors::InvalidNumber.new("Invalid negative numbers: #{negative_numbers.join(', ')}") if integer_array.find(&:negative?)
       integer_array.reject { |number| number >= MAX_PERMITTED_INTEGER }.sum
@@ -26,10 +28,16 @@ class StringCalculator
 
   def parse_string
     match = @string.match(PARSER_REGEX)
-    delimiter, parsed_string = if match
-      [match[1], match[2]]
-    else
-      [DEFAULT_DELIMITER, @string]
-    end
+    return @string unless match
+
+    delimiters_string = match[1]
+    delimeters = delimiters_string.scan(MULTI_DELIMITER_REGEX).flatten
+
+    delimeters = [delimiters_string] if delimeters.empty?
+
+    string = match[2]
+    delimeters.each { |delimiter| string.gsub!(delimiter, DEFAULT_DELIMITER) }
+
+    string
   end
 end
