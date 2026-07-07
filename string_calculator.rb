bcfd67e1c26518a1772f6c22a5f4bb15921aa2e7
Krishna <krishna@incubyte.co>
2026-07-07 11:42:12 +0530

    Support addition for multiline numbers - Green

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,6 +6,9 @@ class StringCalculator
   end
 
   def execute
-    @string.split(DEFAULT_DELIMITER).sum(&:to_i)
+    multiline_strings = @string.split("/n")
+    multiline_strings.map do |string|
+      string.split(DEFAULT_DELIMITER).map(&:to_i)
+    end.flatten.sum
   end
 end
