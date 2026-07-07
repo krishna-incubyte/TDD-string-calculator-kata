c69198fe8af6f266688b0c02390811693a625ec0
Krishna <krishna@incubyte.co>
2026-07-07 12:12:50 +0530

    Add support for custom delimiters - Refactor

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,22 +1,29 @@
 class StringCalculator
   DEFAULT_DELIMITER = ','
   NEWLINE_IDENTIFIER = '/n'
+  PARSER_REGEX = %r{\A//(.+?)\n(.*)}m
 
   def initialize(string)
     @string = string
   end
 
   def execute
-    match = @string.match(%r{\A//(.+?)\n(.*)}m)
-    delimiter, parsed_string = if match
-      [match[1], match[2]]
-    else
-      [DEFAULT_DELIMITER, @string]
-    end
+    delimiter, parsed_string = parse_string
 
     multiline_strings = parsed_string.split(NEWLINE_IDENTIFIER)
     multiline_strings.sum do |string|
       string.split(delimiter).sum(&:to_i)
+    end
+  end
+
+  private
+
+  def parse_string
+    match = @string.match(PARSER_REGEX)
+    delimiter, parsed_string = if match
+      [match[1], match[2]]
+    else
+      [DEFAULT_DELIMITER, @string]
     end
   end
 end
